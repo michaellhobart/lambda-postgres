456e6d5498b2e2c59ba29261b18117ea0ed80658
Michael Hobart <michaelhobart@Michaels-iMac.local>
2018-09-29 16:34:00 -0400

    Added env variables, removed config file and comented main.js

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
+// Import pg for connecting to Postgres Database
 const { Pool } = require('pg')
 
-const config = require('./config.json')
-
+// Create new pool for connecting to DB and set connection params
+// env variables set on lambda function in console
 const pool = new Pool({
-  user: config.user,
-  host: config.host,
-  database: config.database,
-  password: config.password,
-  port: config.port,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
+// Exports handler variable for use with Lambda
 exports.handler = (event, context, callback) => {
+  // Stops Lambda from Timing out
   context.callbackWaitsForEmptyEventLoop = false;
+  // Connect to DB
   pool.connect()
+  // Query DB for all rows in mpc_clients
   .then(client => {
     return client.query('SELECT * FROM mpc_clients')
       .then(res => {
+        // Close Connection to DB
         client.release()
+        // Returns the response from the DB query as the lambda respinse
         callback(null, res.rows)
       })
+      // Handle any errors
       .catch(e => {
         client.release()
         console.log(e.stack)
